5922f934bc1ab3b303bcf86db494717fa5f414a8
kofilolx <kofyamando@gmail.com>
2023-07-20 20:53:42 -0400

    More function -updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-
+#include "holberton.h"
 /**
  * print_most_numbers - Print numbers between 0 to 9 with exceptions.
  *
@@ -10,12 +10,13 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0 i <= 9; i++)
+	i = 0;
+	while (i < 10)
 	{
-		if (i != 2 i != 4)
-		{
+		if (i != 2 && i != 4)
 			_putchar(i + '0');
-		}
+
+		i++;
 	}
 	_putchar('\n');
 }
