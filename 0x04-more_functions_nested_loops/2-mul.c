73fe787efe979680f1e817e96b735ffd03f25d7a
kofilolx <kofyamando@gmail.com>
2023-07-20 20:03:22 -0400

    More function -upt

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * mul - Multiplies two integers
  * @a: Int representing an integer
  * @b: Int representing an integer
- * Return: Always 0.
+ * Return: Return the result of multiplication.
  */
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
