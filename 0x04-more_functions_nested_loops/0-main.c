12a1b658a8dcb0a3b2658f28847fffbbfe8b385f
kofilolx <kofyamando@gmail.com>
2023-07-24 12:00:53 -0400

    0x05-pointers_arrays_strings

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -10,10 +10,10 @@ int main(void)
 	char c;
 
 	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+	_putchar("%c: %d\n", c, _isupper(c));
 
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	_putchar("%c: %d\n", c, _isupper(c));
 
 	return (0);
 }
