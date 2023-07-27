c265da87ace0caa1eac186585b865f0c48f49ceb
kofilolx <kofyamando@gmail.com>
2023-07-27 15:07:20 -0400

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string.
- * @s: string.
- * Return: string.
+ * cap_string - capitalize all words of a string
+ * @s: string input
+ * Return: `s`
  */
+
 char *cap_string(char *s)
 {
-	int i;
+	char punc[] = ",;.!?(){}\n\t\" ";
+	int i, j;
+	int active;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0, active = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
-		|| s[i - 1] == '\t' || s[i - 1] == ','
-		|| s[i - 1] == ';' || s[i - 1] == '!'
-		|| s[i - 1] == '?' || s[i - 1] == '"'
-		|| s[i - 1] == '(' || s[i - 1] == ')'
-		|| s[i - 1] == '{' || s[i - 1] == '}'
-		|| s[i - 1] == '.')
-		&& (s[i] >= 'a' && s[i] <= 'z'))
+		if (s[0] > 96 && s[0] < 123)
+			active = 1;
+		for (j = 0; punc[j] != '\0'; j++)
 		{
-			s[i] = s[i] - 32;
+			if (punc[j] == s[i])
+				active = 1;
 		}
-		else if ((s[0] >= 97 && s[0] <= 122))
+
+		if (active)
 		{
-			s[0] = s[0] - 32;
+			if (s[i] > 96 && s[i] < 123)
+			{
+				s[i] -= 32;
+				active = 0;
+			}
+			else if (s[i] > 64 && s[i] < 91)
+				active = 0;
+			else if (s[i] > 47 && s[i] < 58)
+				active = 0;
 		}
-		else
-		{
-			s[i] = s[i];
-		}
-		i++;
 	}
 	return (s);
 }
-
