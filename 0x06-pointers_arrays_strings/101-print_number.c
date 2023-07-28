8cc7e99171c9215d138554446b2afd3eff853489
kofilolx <kofyamando@gmail.com>
2023-07-28 16:39:26 -0400

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,13 +10,17 @@ void print_number(int n)
 	{
 		_putchar('-');
 		if (n < -9)
+		{
 			print_number(n / -10);
+		}
 		_putchar('0' - n % 10);
 	}
 	else
 	{
 		if (n > 9)
+		{
 			print_number(n / 10);
+		}
 		_putchar(n % 10 + '0');
 	}
 }
