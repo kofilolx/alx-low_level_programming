20c6db059bd7dfecbc66c201945fff5af5346733
kofilolx <kofyamando@gmail.com>
2023-07-28 19:42:44 -0400

    Letter games and net culture

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,12 +9,12 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 52; i++)
+		for (j = 0; input[j] != '\0'; j++)
 		{
 			if (s[i] == input[j])
 			{
