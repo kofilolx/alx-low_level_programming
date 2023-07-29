476251151e20bf9c6764d2c4733024c1dcee40af
kofilolx <kofyamando@gmail.com>
2023-07-28 20:07:34 -0400

    print number update -code fixed

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: argument that takes integer input
+ *
+ * Return: Always 0 (success)
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n < -9)
+		{
+			print_number(num / -10);
+		}
+		_putchar('0' - num % 10);
+	}
+
+	else
+	{
+		if (n > 9)
+		{
+			print_number(num / 10);
+		}
+		_putchar('0' + num % 10);
+	}
+}
