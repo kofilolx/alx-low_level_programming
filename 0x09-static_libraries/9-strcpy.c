93a125be00e393a83515c4f1302dd316e9541b8b
kofilolx <kofyamando@gmail.com>
2023-08-04 00:16:27 -0400

    static n dynamic libraries -update

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src, including \0.
+ * @src: character string
+ * @dest: character string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ *Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i <= len - 1; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
