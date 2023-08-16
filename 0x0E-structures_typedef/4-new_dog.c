d1ddfd8b72fbc09ae453736ebc007d2106ce2952
kofilolx <kofyamando@gmail.com>
2023-08-15 20:33:20 -0400

    structures

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strcpy - copy a string
+ * @s: string to copy
+ * Return: pointer to string copy
+ */
+char *_strcpy(char *s)
+{
+	int i;
+	char *ptr;
+
+	for (i = 0; s[i]; i++)
+		;
+	ptr = malloc(i + 1);
+	if (ptr)
+	{
+		for (; i >= 0; i--)
+			ptr[i] = s[i];
+	}
+	return (ptr);
+}
+
+/**
+ * new_dog - creates a new profile for dog struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Return: dog struct or NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pt;
+
+	pt = malloc(sizeof(*pt));
+	if (pt == NULL)
+		return (NULL);
+	pt->name = _strcpy(name);
+	if (pt->name == NULL)
+	{
+		free(pt);
+		return (NULL);
+	}
+	pt->age = age;
+	pt->owner = _strcpy(owner);
+	if (pt->owner == NULL)
+	{
+		free(pt->name);
+		free(pt);
+		return (NULL);
+	}
+	return (pt);
+}
