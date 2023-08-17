f5b222d16ef5d0a9fd1a10c2b823282bf90b585c
kofilolx <kofyamand@gmail.com>
2023-08-16 19:52:39 -0700

    calculator functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+/**
+ * main - compute simple math operations
+ * @argc: number of arguments in argv
+ * @argv: arguments containing 2 numbers and operator
+ * Return: Success (0), Error (98)
+ */
+ 
+int main(int argc, char *argv[])
+{
+	int (*ptr)(int, int);
+
+	if (argc != 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	ptr = get_op_func(argv[2]);
+
+	if (!ptr)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+	printf("%d\n", ptr(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
