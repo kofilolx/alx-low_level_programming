00827eaf5e69a66338034c48286e080398bad9ec
kofilolx <kofyamando@gmail.com>
2023-08-29 21:41:46 -0400

    singly linked

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
+#include <string.h>
+#include <stdlib.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include<stdlib.h>
 
+int (*get_op_func(char *s))(int, int);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
