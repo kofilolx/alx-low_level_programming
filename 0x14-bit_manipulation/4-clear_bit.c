5eadb8211b90a789b8eaab086d0f01bb8411163d
kofilolx <kofyamando@gmail.com>
2023-08-31 21:36:28 -0400

    BSR Bit Set &wq ~(set)

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 1.
+ * at a given position or index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ * Return: Success 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int size;
+
+	size = (sizeof(n) * 8);
+	if (index > size)
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
