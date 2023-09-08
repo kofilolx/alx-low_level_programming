c56b32491811706e9a009bc7721eb9451dec6938
kofilolx <kofyamando@gmail.com>
2023-09-08 01:52:23 -0400

    file handling

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,25 +8,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int fd, wr, b = 0;
+	int fd, wr, b = 0;
 
-        if (!filename)
-                return (-1);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-        fd = open(filename, O_WRONLY | O_APPEND);
-        if (fd < 0)
-                return (-1);
-
-        if (text_content)
-        {
-                while (text_content[b])
-                        b++;
-                wr = write(fd, text_content, b);
-                if (wr != b)
-                        return (-1);
-        }
-
-        close(fd);
-
-        return (1);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[b])
+			b++;
+		wr = write(fd, text_content, b);
+		if (wr != b)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
