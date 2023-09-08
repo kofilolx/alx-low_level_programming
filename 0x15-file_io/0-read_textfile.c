1686eff2afcdcfc08513592e5dae0fae9d7f544b
kofilolx <kofyamando@gmail.com>
2023-09-08 01:36:40 -0400

    low level Programming

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,37 +8,37 @@
  * Return: return number of letters printed
  */
 
-ssize_t read_textfile(const char filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd, rd, wr;
-        char *buffer;
+	int fd;
+	ssize_t rd, wr;
+	char *buffer;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == 0 || fd < 0)
-                return (0);
-        buffer = malloc(sizeof(char) * letters);
-        if (!buffer)
-        {
-                close(fd);
-                return (0);
-        }
-        rd = read(fd, buffer, letters);
-        
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	rd = read(fd, buffer, letters);
 	close(fd);
-        if (rd < 0)
-        {
-                free(buffer);
-                return (0);
-        }
-        wr = write(STDOUT_FILENO, buffer, rd);
-        free(buffer);
 
-        if (rd != wr)
-                return (0);
+	if (rd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-        return (rd);
+	wr = write(STDOUT_FILENO, buffer, rd);
+	free(buffer);
+	if (rd != wr)
+		return (0);
+	return (wr);
 }
-
